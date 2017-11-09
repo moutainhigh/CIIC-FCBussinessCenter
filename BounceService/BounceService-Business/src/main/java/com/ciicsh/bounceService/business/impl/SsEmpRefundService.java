b255330b304ade14ff10dde42ce48a099634f28c
houwanhua <houwanhua@ciicsh.com>
2017-11-09 19:34:14 +0800

    add new service

---
+++ BounceService/BounceService-Business/src/main/java/com/ciicsh/bounceService/business/impl/SsEmpRefundService.java
@@ -0,0 +1,27 @@
+package com.ciicsh.bounceService.business.impl;
+
+import com.ciicsh.bounceService.business.ISsEmpRefundService;
+import com.ciicsh.bounceService.dao.SsEmpRefundMapper;
+import com.ciicsh.business.entity.SsEmpRefund;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by houwanhua on 2017/11/8.
+ */
+@Service
+@Transactional
+public class SsEmpRefundService implements ISsEmpRefundService {
+    @Autowired
+    private SsEmpRefundMapper ssEmpRefundMapper;
+
+    /**
+     * 根据雇员任务单id获取雇员社保退账受理信息
+     * @param employeeTaskId 雇员任务单id
+     * @return 雇员社保退账受理信息
+     * */
+    public SsEmpRefund getSsEmpRefundByEmployeeTaskId(String employeeTaskId){
+        return ssEmpRefundMapper.getSsEmpRefundByEmployeeTaskId(employeeTaskId);
+    }
+}
---
+++ ConsignationService/ConsignationService-Dao/src/main/java/com/ciicsh/consignationservice/dao/SsEmpRefundMapper.java
@@ -0,0 +1,19 @@
+package com.ciicsh.consignationservice.dao;
+
+import com.ciicsh.business.entity.SsEmpRefund;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by houwanhua on 2017/11/8.
+ */
+@Mapper
+@Component
+public interface SsEmpRefundMapper {
+    /**
+     * 根据雇员任务单id获取雇员社保退账受理信息
+     * @param employeeTaskId 雇员任务单id
+     * @return 雇员社保退账受理信息
+     * */
+    SsEmpRefund getSsEmpRefundByEmployeeTaskId(String employeeTaskId);
+}
