da7c6c34c2308930728afe4806cd7bafaa24eb97
蒋天宁 <jiangtianning@ciicsh.com>
2018-01-04 15:41:39 +0800

    add new po and mapper

---
+++ PayrollCal/CommandService/CommandService-Dao/src/main/java/com/ciicsh/gto/salarymanagementcommandservice/dao/PrPayrollGroupTemplateHistoryMapper.java
@@ -0,0 +1,16 @@
+package com.ciicsh.gto.salarymanagementcommandservice.dao;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.ciicsh.gto.salarymanagement.entity.po.PrPayrollGroupTemplateHistoryPO;
+
+/**
+ * <p>
+  * 薪资组模板版本历史记录表 Mapper 接口
+ * </p>
+ *
+ * @author Neo Jiang
+ * @since 2018-01-04
+ */
+public interface PrPayrollGroupTemplateHistoryMapper extends BaseMapper<PrPayrollGroupTemplateHistoryPO> {
+
+}
