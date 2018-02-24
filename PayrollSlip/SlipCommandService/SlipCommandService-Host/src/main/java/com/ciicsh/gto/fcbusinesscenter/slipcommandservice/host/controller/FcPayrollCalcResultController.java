367df876b8374ce05139739176bcadd72d535ed5
takafan <takafan@163.com>
2018-02-24 13:50:34 +0800

    add gaorong api

--- PayrollSlip/SlipCommandService/SlipCommandService-Host/src/main/java/com/ciicsh/gto/fcbusinesscenter/slipcommandservice/host/controller/FcPayrollCalcResultController.java
+++ PayrollSlip/SlipCommandService/SlipCommandService-Host/src/main/java/com/ciicsh/gto/fcbusinesscenter/slipcommandservice/host/controller/FcPayrollCalcResultController.java
@@ -1,9 +1,12 @@
 package com.ciicsh.gto.fcbusinesscenter.slipcommandservice.host.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.ciicsh.gt1.FileHandler;
 import com.ciicsh.gto.fcbusinesscenter.slipcommandservice.api.JsonResult;
 
 import com.ciicsh.gto.fcbusinesscenter.slipcommandservice.business.FcPayrollCalcResultService;
+import com.ciicsh.gto.fcbusinesscenter.slipcommandservice.entity.po.FcPayrollCalcResultPO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -59,5 +67,72 @@ public class FcPayrollCalcResultController {
     public JsonResult listPayrollTypes(@RequestBody Map<String, Object> params) {
         return JsonResult.success(fcPayrollCalcResultService.listPayrollTypes(params));
     }
+
+    @RequestMapping(value = "/SalaryQuery")
+    public JsonResult SalaryQuery(HttpServletRequest request) {
+        String empId = request.getParameter("EmployeeID");
+        if (empId == null || empId.equals("")) {
+            return JsonResult.faultMessage("缺少参数EmployeeID");
+        }
+
+        ArrayList<Map<String, Object>> list = new ArrayList();
+
+        Map<String, Object> params = new HashMap<String, Object>(){
+            {
+                put("empId", empId);
+            }
+        };
+
+
+        for (FcPayrollCalcResultPO po : fcPayrollCalcResultService.listFcPayrollCalcResults(params)) {
+
+            Map<String, Object> ele = new HashMap<String, Object>(){
+                {
+                    put("SalaryID", po.getFcPayrollCalcResultId());
+                    put("SalaryMonth", po.getPersonnelIncomeYearMonth());
+                    put("Salary", po.getPersonnelIncomeNetPay());
+                }
+            };
+
+            list.add(ele);
+        }
+
+        return JsonResult.success(list);
+    }
+
+    @RequestMapping(value = "/DetailsSalaryQuery")
+    public JsonResult DetailsSalaryQuery(HttpServletRequest request) {
+        String fcPayrollCalcResultId = request.getParameter("SalaryID");
+        if (fcPayrollCalcResultId == null || fcPayrollCalcResultId.equals("")) {
+            return JsonResult.faultMessage("缺少参数SalaryID");
+        }
+
+        Map<String, Object> params = new HashMap<String, Object>(){
+            {
+                put("fcPayrollCalcResultId", fcPayrollCalcResultId);
+            }
+        };
+
+        FcPayrollCalcResultPO po = fcPayrollCalcResultService.getFcPayrollCalcResult(params);
+
+        ArrayList<Map<String, Object>> list = new ArrayList();
+
+        JSONObject hash = JSON.parseObject(po.getSalaryCalcResultItems());
+
+        for (String key : hash.keySet()) {
+            Map<String, Object> item = new HashMap<String, Object>(){
+                {
+                    put("ItemName", key);
+                    put("ItemValue", hash.get(key));
+                }
+            };
+            list.add(item);
+        }
+
+        return JsonResult.success(list);
+    }
+
+
+
 
 }
