93c37f5e1ebfae9d94d0781a7edb27b3306d0249
wuhua <wuhua@ciicsh.com>
2018-03-13 14:14:55 +0800

    调整自动填充后台逻辑

--- Tax/TaxCommandService/CommandService-Host/src/main/java/com/ciicsh/gto/fcbusinesscenter/tax/commandservice/host/handler/TaxMetaObjectHandler.java
+++ Tax/TaxCommandService/CommandService-Host/src/main/java/com/ciicsh/gto/fcbusinesscenter/tax/commandservice/host/handler/TaxMetaObjectHandler.java
@@ -1,18 +1,42 @@
 package com.ciicsh.gto.fcbusinesscenter.tax.commandservice.host.handler;
 
 import com.baomidou.mybatisplus.mapper.MetaObjectHandler;
+import com.ciicsh.gto.fcbusinesscenter.tax.commandservice.host.intercept.LoginInfoHolder;
+import com.ciicsh.gto.identityservice.api.dto.response.UserInfoResponseDTO;
 import org.apache.ibatis.reflection.MetaObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 /**
  * @author wuhua
  */
 public class TaxMetaObjectHandler extends MetaObjectHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(TaxMetaObjectHandler.class);
+
     public void insertFill(MetaObject metaObject) {
 
+        LocalDateTime llt = LocalDateTime.now();
+
+        try {
+            //登录信息
+            UserInfoResponseDTO userInfoResponseDTO = LoginInfoHolder.get().getResult().getObject();
+            //创建人
+            setFieldValByName("createdBy", userInfoResponseDTO.getLoginName(), metaObject);
+            //修改人
+            setFieldValByName("modifiedBy", userInfoResponseDTO.getLoginName(), metaObject);
+        } catch (Exception e) {
+            logger.error("TaxMetaObjectHandler.insertFill "+e.getMessage());
+            setFieldValByName("createdBy", "", metaObject);
+            setFieldValByName("modifiedBy", "", metaObject);
+        }finally {
+            //创建时间
+            setFieldValByName("createdTime",llt , metaObject);
+            //修改时间
+            setFieldValByName("modifiedTime", llt, metaObject);
+        }
     }
 
     /**
@@ -21,7 +45,20 @@ public class TaxMetaObjectHandler extends MetaObjectHandler {
      */
     @Override
     public void updateFill(MetaObject metaObject) {
-        //修改时间
-        setFieldValByName("modifiedTime", LocalDateTime.now(), metaObject);
+
+        LocalDateTime llt = LocalDateTime.now();
+
+        try {
+            //登录信息
+            UserInfoResponseDTO userInfoResponseDTO = LoginInfoHolder.get().getResult().getObject();
+            //修改人
+            setFieldValByName("modifiedBy", userInfoResponseDTO.getLoginName(), metaObject);
+        } catch (Exception e) {
+            logger.error("TaxMetaObjectHandler.updateFill " + e.getMessage());
+            setFieldValByName("modifiedBy", "", metaObject);
+        }finally {
+            //修改时间
+            setFieldValByName("modifiedTime", llt, metaObject);
+        }
     }
 }
