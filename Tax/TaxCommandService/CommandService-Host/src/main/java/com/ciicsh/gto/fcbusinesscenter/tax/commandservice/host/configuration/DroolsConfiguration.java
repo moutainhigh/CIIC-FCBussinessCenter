764876ff4d17986c1dbb1ce45222a8f8c5dbe587
wuhua <wuhua@ciicsh.com>
2018-04-03 15:37:11 +0800

    后台服务逻辑调整（新增drools规则）

---
+++ Tax/TaxCommandService/CommandService-Host/src/main/java/com/ciicsh/gto/fcbusinesscenter/tax/commandservice/host/configuration/DroolsConfiguration.java
@@ -0,0 +1,9 @@
+package com.ciicsh.gto.fcbusinesscenter.tax.commandservice.host.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource({"classpath:drools-spring-config.xml"})
+public class DroolsConfiguration {
+}
