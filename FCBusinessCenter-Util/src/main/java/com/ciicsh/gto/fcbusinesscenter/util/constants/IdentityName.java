a26fe75ed0c375cac513fda43c1c306f3859b0dc
bill <hubing@ciicsh.com>
2018-05-10 09:41:14 +0800

    update

---
+++ FCBusinessCenter-Util/src/main/java/com/ciicsh/gto/fcbusinesscenter/util/constants/IdentityName.java
@@ -0,0 +1,14 @@
+package com.ciicsh.gto.fcbusinesscenter.util.constants;
+
+/**
+ * Created by bill on 18/5/9.
+ */
+public class IdentityName {
+
+    public static final String EMPLOYEE_ID = "employee_id";
+
+    public static final String EMPLOYEE_NAME = "employee_name";
+
+    public static final String ID_NUM = "id_num";
+
+}
