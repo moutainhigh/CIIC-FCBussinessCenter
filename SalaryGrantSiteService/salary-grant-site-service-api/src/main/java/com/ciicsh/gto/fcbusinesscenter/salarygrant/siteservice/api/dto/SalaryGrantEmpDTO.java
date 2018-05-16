ca9b27925ecfc0a2f8e19a211b0ff608bf953937
chenpengbo <chenpengbo@ciicsh.com>
2018-05-16 10:03:40 +0800

    修改导出处理

--- SalaryGrantSiteService/salary-grant-site-service-api/src/main/java/com/ciicsh/gto/fcbusinesscenter/salarygrant/siteservice/api/dto/SalaryGrantEmpDTO.java
+++ SalaryGrantSiteService/salary-grant-site-service-api/src/main/java/com/ciicsh/gto/fcbusinesscenter/salarygrant/siteservice/api/dto/SalaryGrantEmpDTO.java
@@ -80,6 +80,10 @@ public class SalaryGrantEmpDTO extends PagingDTO {
      */
     private String countryName;
     /**
+     * 发放状态:0-正常，1-手动暂缓，2-自动暂缓，3-退票，4-部分发放
+     */
+    private Integer grantStatus;
+    /**
      * 发放状态中文
      */
     private String grantStatusName;
