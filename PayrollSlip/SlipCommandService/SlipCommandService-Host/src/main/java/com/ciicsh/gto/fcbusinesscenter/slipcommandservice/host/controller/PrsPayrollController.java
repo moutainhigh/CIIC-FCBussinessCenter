30af827fa6bdd04aabb3a036087cb42d086637b7
takafan <takafan@163.com>
2018-06-04 16:08:22 +0800

    update payroll module

--- PayrollSlip/SlipCommandService/SlipCommandService-Host/src/main/java/com/ciicsh/gto/fcbusinesscenter/slipcommandservice/host/controller/PrsPayrollController.java
+++ PayrollSlip/SlipCommandService/SlipCommandService-Host/src/main/java/com/ciicsh/gto/fcbusinesscenter/slipcommandservice/host/controller/PrsPayrollController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
 import java.util.Map;
 
 /**
@@ -43,6 +45,11 @@ public class PrsPayrollController {
     @RequestMapping(value = "/addPrsPayroll")
     public JsonResult add(@RequestBody Map<String, Object> params) {
         return JsonResult.success(prsPayrollService.addPrsPayroll(params));
+    }
+
+    @RequestMapping(value = "/addPrsPayrolls")
+    public JsonResult adds(@RequestBody ArrayList<Map<String, Object>> objs) {
+        return JsonResult.success(prsPayrollService.addPrsPayrolls(objs));
     }
 
     @RequestMapping(value = "/updatePrsPayroll")
