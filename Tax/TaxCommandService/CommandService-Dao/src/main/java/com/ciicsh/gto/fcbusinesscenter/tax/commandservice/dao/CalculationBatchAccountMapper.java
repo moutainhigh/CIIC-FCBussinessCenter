98ed5d0f9386b87e600617e84912e731c7432792
wuhua <wuhua@ciicsh.com>
2018-07-04 15:35:26 +0800

    后台逻辑调整

---
+++ Tax/TaxCommandService/CommandService-Dao/src/main/java/com/ciicsh/gto/fcbusinesscenter/tax/commandservice/dao/CalculationBatchAccountMapper.java
@@ -0,0 +1,12 @@
+package com.ciicsh.gto.fcbusinesscenter.tax.commandservice.dao;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.ciicsh.gto.fcbusinesscenter.tax.entity.po.CalculationBatchAccountPO;
+
+/**
+ * @author wuhua
+ */
+public interface CalculationBatchAccountMapper extends BaseMapper<CalculationBatchAccountPO> {
+
+
+}
