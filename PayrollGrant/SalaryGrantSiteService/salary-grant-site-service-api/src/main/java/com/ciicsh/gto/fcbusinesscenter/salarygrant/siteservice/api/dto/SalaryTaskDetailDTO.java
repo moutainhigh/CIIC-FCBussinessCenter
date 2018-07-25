2911bf2873ad603e7b7644756b1dc161f2353b0a
chenpengbo <chenpengbo@ciicsh.com>
2018-07-25 19:27:42 +0800

    GT1-5362

--- PayrollGrant/SalaryGrantSiteService/salary-grant-site-service-api/src/main/java/com/ciicsh/gto/fcbusinesscenter/salarygrant/siteservice/api/dto/SalaryTaskDetailDTO.java
+++ PayrollGrant/SalaryGrantSiteService/salary-grant-site-service-api/src/main/java/com/ciicsh/gto/fcbusinesscenter/salarygrant/siteservice/api/dto/SalaryTaskDetailDTO.java
@@ -47,6 +47,10 @@ public class SalaryTaskDetailDTO extends PagingDTO implements Serializable {
      */
     private Integer grantStatus;
     /**
+     * 发放方式:1-中智上海账户、2-中智代发（委托机构）、3-中智代发（客户账户）、4-客户自行
+     */
+    private String grantMode;
+    /**
      * 薪资发放任务单信息
      */
     private SalaryGrantDetailDTO task;
